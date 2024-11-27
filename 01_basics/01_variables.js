89f30ca6b8f0d1d188c8f58f2dc6cfece6fb738f
Sanjeev Pathak <sanjeev.pathak@edu.novia.fi>
2024-11-27 03:11:35 +0000

    basics practices

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 132;
+let accountEmail = "sanjeev";
+var accountPassword = "12345";
+accountCity = "helsinki";
+
+accountCity = "Kerava";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+for 
+
